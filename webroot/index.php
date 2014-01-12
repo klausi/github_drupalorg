35b367ba3fa453ddbc9baa85baf866900b6d456f
Klaus Purer <klaus.purer@gmail.com>
2014-01-12 23:14:22 +0100

    Implemented creation of new pull requests.

--- webroot/index.php
+++ webroot/index.php
@@ -20,25 +20,38 @@ if ($_SERVER['HTTP_X_HUB_SIGNATURE'] !== "sha1=$signature") {
 
 $payload = json_decode($_POST['payload']);
 
+logger(print_r($payload, TRUE));
+
+$pull_request_url = $payload->pull_request->html_url;
+
 switch ($payload->action) {
   case 'synchronize':
-    $branch_name = $payload->pull_request->head->ref;
-    $matches = array();
-
-    // Only look at branch names that end in an issue number.
-    if (!preg_match('/([0-9]+)$/', $branch_name, $matches)) {
-      exit;
-    }
-
-    $diff_url = $payload->pull_request->diff_url;
-    // Download the patch.
-    $file = download_file($diff_url, "$branch_name.patch");
-
-    post_comment($matches[0], 'test comment', $file);
-
-    // Cleanup: remove the downloaded file and the temporary directory it is in.
-    unlink($file);
-    rmdir(dirname($file));
-
+    $comment = 'Some commits were pushed to the <a href="' . $pull_request_url . '">pull request</a>.';
     break;
+
+  case 'opened':
+    $comment = 'A <a href="' . $pull_request_url . '">new pull request</a> was opened by <a href="' . $payload->pull_request->user->html_url . '">' . $payload->pull_request->user->login . '</a> for this issue.';
+    break;
+
+  default:
+    // Unknown action, so we just exit.
+    exit;
 }
+
+$branch_name = $payload->pull_request->head->ref;
+$matches = array();
+
+// Only look at branch names that end in an issue number.
+if (!preg_match('/([0-9]+)$/', $branch_name, $matches)) {
+  exit;
+}
+
+$diff_url = $payload->pull_request->diff_url;
+// Download the patch.
+$file = download_file($diff_url, "$branch_name.patch");
+
+post_comment($matches[0], $comment, $file);
+
+// Cleanup: remove the downloaded file and the temporary directory it is in.
+unlink($file);
+rmdir(dirname($file));
