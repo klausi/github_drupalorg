05461b12c6e658d64bdc8377747a480ddad6d8ae
Klaus Purer <klaus.purer@gmail.com>
2014-01-17 16:17:41 +0100

    Implemented comment handling.

--- utility.php
+++ utility.php
@@ -12,6 +12,11 @@ require 'vendor/autoload.php';
  *   The received data.
  */
 function handle_pull_request($payload) {
+  $issue_number = get_issue_number($payload->pull_request);
+  if (!$issue_number) {
+    exit;
+  }
+
   $pull_request_url = $payload->pull_request->html_url;
 
   switch ($payload->action) {
@@ -28,19 +33,11 @@ function handle_pull_request($payload) {
       exit;
   }
 
-  $branch_name = $payload->pull_request->head->ref;
-  $matches = array();
-
-  // Only look at branch names that end in an issue number.
-  if (!preg_match('/([0-9]+)$/', $branch_name, $matches)) {
-    exit;
-  }
-
   $diff_url = $payload->pull_request->diff_url;
   // Download the patch.
   $file = download_file($diff_url, "$branch_name.patch");
 
-  post_comment($matches[0], $comment, $file);
+  post_comment($issue_number, $comment, $file);
 
   // Cleanup: remove the downloaded file and the temporary directory it is in.
   unlink($file);
@@ -54,6 +51,18 @@ function handle_pull_request($payload) {
  *   The received data.
  */
 function handle_comment($payload) {
+  global $oauth_token;
+  $client = new GuzzleClient();
+
+  // Use the OAuth token to access the API, to have a higher rate limit.
+  $response = $client->get($payload->comment->pull_request_url . "?access_token=$oauth_token")->send();
+  $pull_request = json_decode($response->getBody());
+  $issue_number = get_issue_number($pull_request);
+
+  if (!$issue_number) {
+    exit;
+  }
+
   $comment = 'A <a href="' . $payload->comment->html_url . '">new comment</a> has been posted by <a href="' . $payload->comment->user->html_url . '">' . $payload->comment->user->login . '</a> on ' . $payload->comment->html_url . "\n";
   $comment .= 'Path: ' . $payload->comment->path . "\n";
   $comment .= "<code>\n";
@@ -61,6 +70,7 @@ function handle_comment($payload) {
   $comment .= "</code>\n";
   $comment .= $payload->comment->body;
 
+  post_comment($issue_number, $comment);
 }
 
 /**
@@ -71,9 +81,9 @@ function handle_comment($payload) {
  * @param string $comment
  *   Text for the comment field.
  * @param string $patch
- *   File name to attach as patch.
+ *   Optional, file name to attach as patch.
  */
-function post_comment($issue_id, $comment, $patch) {
+function post_comment($issue_id, $comment, $patch = NULL) {
   static $client;
   if (!$client) {
     // Perform a user login.
@@ -99,19 +109,21 @@ function post_comment($issue_id, $comment, $patch) {
   // summary changes that we don't want to touch.
   $form['body[und][0][value]']->setValue(html_entity_decode($form->get('body[und][0][value]')->getValue(), ENT_QUOTES, 'UTF-8'));
 
-  // There can be uploaded files already, so we need to iterate to the most
-  // recent file number.
-  $file_nr = 0;
-  while (!isset($form["files[field_issue_files_und_$file_nr]"])) {
-    $file_nr++;
-  }
-  $form["files[field_issue_files_und_$file_nr]"]->upload($patch);
+  if ($patch) {
+    // There can be uploaded files already, so we need to iterate to the most
+    // recent file number.
+    $file_nr = 0;
+    while (!isset($form["files[field_issue_files_und_$file_nr]"])) {
+      $file_nr++;
+    }
+    $form["files[field_issue_files_und_$file_nr]"]->upload($patch);
 
-  $status = $form['field_issue_status[und]']->getValue();
+    $status = $form['field_issue_status[und]']->getValue();
 
-  // Set the issue to "needs review" if it is not alreay "needs review" or RTBC.
-  if ($status != 8 && $status != 14) {
-    $form['field_issue_status[und]']->setValue(8);
+    // Set the issue to "needs review" if it is not alreay "needs review" or RTBC.
+    if ($status != 8 && $status != 14) {
+      $form['field_issue_status[und]']->setValue(8);
+    }
   }
 
   $client->submit($form);
@@ -152,4 +164,25 @@ function download_file($url, $file_name) {
   $path = "$temp_file/$file_name";
   file_put_contents($path, $response->getBody());
   return $path;
+}
+
+/**
+ * Extracts a potential issue number from the received pull request object.
+ *
+ * @param object $pull_request
+ *   The pull request object.
+ *
+ * @return int|false
+ *   Returns the issue number or FALSE if no number could be extracted.
+ */
+function get_issue_number($pull_request) {
+  $branch_name = $pull_request->head->ref;
+  $matches = array();
+
+  // Only look at branch names that end in an issue number with a least 4
+  // digits.
+  if (preg_match('/([0-9]{4}[0-9]*)$/', $branch_name, $matches)) {
+    return $matches[0];
+  }
+  return FALSE;
 }
